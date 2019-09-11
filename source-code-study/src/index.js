c6259b2f03144e58e55516325f58378f30f492df
sven-ren <657977587@qq.com>
2019-09-11 09:12:51 +0800

    react源码研究（1）

---
+++ source-code-study/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import React from './packages/react';
+import ReactDOM from 'react-dom';
+// import ReactDOM from './packages/react-dom';
+// import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
